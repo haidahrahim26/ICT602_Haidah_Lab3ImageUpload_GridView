2b92ef55693eb18e8ca80d20e10aa1a3a333a881
haidahrahim26 <haidah266000@gmail.com>
2023-12-22 22:56:57 +0800

    My lab 3 Firebase

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ICT602CDCS2703b_HaidahLab3_ImageUpload"
+include(":app")
+ 
